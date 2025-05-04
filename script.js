a04601dd704135bdfdc57328b234f4fd2a782979
Web-Code <ahmedwahaj317@gmail.com>
2025-05-04 12:38:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,8 @@ window.addEventListener('scroll', function () {
   const nav = document.querySelector('nav');
   if (window.scrollY > 50) {
    nav.classList.add('scrolled');
+   } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
