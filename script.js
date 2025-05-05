b8689e74432bea47250f95cc7e223866b8e85e7b
Web-Code <ahmedwahaj317@gmail.com>
2025-05-05 17:59:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,9 @@ window.addEventListener('scroll', function () {
 
 let slideIndex = 1;
 showSlides(slideIndex);
+
 function plusSlides(n) {
-             showSlides(slideIndex += n);
+ showSlides(slideIndex += n);
 }
 
+function currentSlide(n) {
